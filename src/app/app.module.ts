d4b26cd870465af2856537b2361d27470d40522e
Luis Rafael Rivera <rafael@bambu-mobile.com>
2017-08-18 14:29:24 -0500

    rafa
    error de compilacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {MdGridListModule} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MdGridListModule,
     NoopAnimationsModule
   ],
   providers: [],
